9365fbeeb78a33834cb56a10fe8d8503e12697e3
galaxlabs <galaxylab2020@gmail.com>
2025-12-21 10:43:44 +0000

    phone update

--- client/src/lib/data.ts
+++ client/src/lib/data.ts
@@ -197,8 +197,8 @@ export const locations = [
 
 export const companyInfo = {
   name: "Al-Haramain Transport",
-  phone: "+966-50-123-4567",
-  whatsapp: "+966501234567",
+  phone: "+966-58-235-6295",
+  whatsapp: "+966582356295",
   email: "info@alharamaintransport.com",
   hours: "24/7 - Always Available",
   rating: 4.8,
